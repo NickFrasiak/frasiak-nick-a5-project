f4e3dd124aa369fd998259e3f99147b6c9d2d4ec
Nicholas Frasiak <000959762@mohawkcollege.ca>
2024-12-12 13:44:19 -0500

    Add screen wrapping

--- Asteroids/Scripts/Player.cs
+++ Asteroids/Scripts/Player.cs
@@ -3,35 +3,58 @@ using System;
 
 public partial class Player : Node2D
 {
-	[Export]
-	public float speed = 200;
-	[Export]
-	public float rotationSpeed = 5;
+    [Export]
+    public float speed = 300;
+    [Export]
+    public float rotationSpeed = 5;
 
-	private Vector2 velocity = Vector2.Zero;
-	public override void _Ready()
-	{
+    private Vector2 velocity = Vector2.Zero;
+    public override void _Ready()
+    {
 
-	}
-	public override void _Process(double delta)
-	{
-		//player rotation
-		if (Input.IsActionPressed("right"))
-		{
+    }
+    public override void _Process(double delta)
+    {
+        //player rotation
+        if (Input.IsActionPressed("right"))
+        {
             Rotation += rotationSpeed * (float)delta;
         }
 
-		if (Input.IsActionPressed("left"))
-		{
+        if (Input.IsActionPressed("left"))
+        {
             Rotation -= rotationSpeed * (float)delta;
         }
 
-		//Movement
-		if (Input.IsActionPressed("forward"))
-		{
-			Position += (Transform.X * speed) * (float)delta;
+        //Movement
+        if (Input.IsActionPressed("forward"))
+        {
+            Position += (Transform.X * speed) * (float)delta;
         }
-	}
+
+        //var viewport_size = GetViewportRect().Size;
+        if (Position.X < 0)
+        {
+            Position = new Vector2(GetViewport().GetVisibleRect().Size.X, Position.Y);
+        }
+        else if (Position.X > GetViewport().GetVisibleRect().Size.X)
+        {
+            Position = new Vector2(0, Position.Y);
+
+
+        }
+
+        if (Position.Y < 0)
+        {
+            Position = new Vector2(Position.X, GetViewport().GetVisibleRect().Size.Y);
+        }
+        else if (Position.Y > GetViewport().GetVisibleRect().Size.Y)
+        {
+            Position = new Vector2(Position.X, 0);
+
+
+        }
+    }
 
 }
 
